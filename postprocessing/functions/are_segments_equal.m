e5d7b8959f3902a47e245ce5a5ef50a8c85ecd87
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-11-06 12:12:20 +0100

    Minor improvement

--- postprocessing/functions/are_segments_equal.m
+++ postprocessing/functions/are_segments_equal.m
@@ -1,6 +1,4 @@
 function equal = are_segments_equal(segment1, segment2)
-    % DOESN'T WORK CORRECTLY
-
     % Check if the two segments are equal, considering opposite directions
     equal = isequal(segment1, segment2) || isequal(segment1, flipud(segment2));
 end
