9c5ab4866c03631a8bd5ee37b080b072ddf6ff1b
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2026-03-08 12:20:21 +0100

    Add experiment designer GUI tool (tools/editor.html)

--- source/run_sim_viz.bat
+++ source/run_sim_viz.bat
@@ -1,6 +1,6 @@
 @echo off
 set EXP=%1
-if "%EXP%"=="" set EXP=hall
+if "%EXP%"=="" set EXP=my_experiment
 echo Running experiment: %EXP%
 .\build\WASAbiApp.exe --experiment %EXP% --mode sim-viz
 pause
